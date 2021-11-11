7e216e793869408a8bb5fb7dafeaa9392c761298
Tormod Ulsberg <tormod.ulsberg@gmail.com>
2021-11-11 15:03:06 +0100

    Update proxy.js

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -6,7 +6,7 @@ const cors = initMiddleware(Cors(corsOptionsDelegate))
 const proxy = async (req, res) => {
   await cors(req, res)
   const headers = {}
-  const validHeaders = ['authorization', 'content-type', 'client-id', 'select-record', 'api-version']
+  const validHeaders = ['authorization', 'content-type', 'client-id', 'select-record', 'api-version', 'x-url']
   validHeaders.forEach(header => {
     if (req.headers[header]) {
       headers[header] = req.headers[header]
